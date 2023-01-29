4c0ee72156d3ce9fda251493303fbcb27dab312f
Sabyasachi Rakshit <69518941+sabyasachirakshit@users.noreply.github.com>
2023-01-29 12:09:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const path=require("path");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname, "")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 let messages = [];
 
