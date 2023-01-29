9a0ffa5e3b41e308504047daacfe5f1d3af6c357
Sabyasachi Rakshit <69518941+sabyasachirakshit@users.noreply.github.com>
2023-01-29 12:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path=require("path");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname, "")));
+app.use(express.static(path.join(__dirname, "logic.js")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
