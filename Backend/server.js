25409b386f04f3bcba0fd06a67d3b51c6dcc2dc1
sabyasachirakshit <sabyasachi@121connection.com>
2023-01-29 11:56:25 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
-});
 
 let messages = [];
 
